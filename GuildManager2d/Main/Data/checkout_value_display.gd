14e59d6a60ee3bae4af1a0e90fca39a3033baa40
John Cester <stu236155@mail.uni-kiel.de>
2024-12-28 16:52:15 +0100

    ADDED: paid/value/change display for checkout not complete, NEXT: complete it

---
+++ GuildManager2d/Main/Data/checkout_value_display.gd
@@ -0,0 +1,28 @@
+extends MarginContainer
+
+@onready var paid_gold_value_label: Label
+@onready var paid_silver_value_label: Label
+@onready var paid_copper_value_label: Label
+@onready var change_gold_icon: TextureRect
+@onready var change_silver_icon: TextureRect
+@onready var change_copper_icon: TextureRect
+
+@onready var value_gold_value_label: Label
+@onready var value_silver_value_label: Label
+@onready var value_copper_value_label: Label
+@onready var change_gold_icon: TextureRect
+@onready var change_silver_icon: TextureRect
+@onready var change_copper_icon: TextureRect
+
+@onready var change_gold_value_label: Label
+@onready var change_silver_value_label: Label
+@onready var change_copper_value_label: Label
+@onready var change_gold_icon: TextureRect
+@onready var change_silver_icon: TextureRect
+@onready var change_copper_icon: TextureRect
+
+func set_paid_display(value):
+	
+func set_value_display(value):
+	
+func set_change_display(value):
